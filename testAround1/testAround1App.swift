7a25169687301998c02de52a28e1938a551604f2
neilbronfin <nbronfin21@gmail.com>
2024-02-27 10:17:59 -0600

    Initial Commit

---
+++ testAround1/testAround1App.swift
@@ -0,0 +1,17 @@
+//
+//  testAround1App.swift
+//  testAround1
+//
+//  Created by Neil Bronfin on 2/27/24.
+//
+
+import SwiftUI
+
+@main
+struct testAround1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
